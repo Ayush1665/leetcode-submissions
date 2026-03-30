5beb4dc8a62cdc5f4d1049d183c2f11259d87a95
Ayush Singla <101383376+Ayush1665@users.noreply.github.com>
2026-03-30 21:34:14 +0530

    Time: 1 ms (78.21%) | Memory: 46.6 MB (87.88%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -6,6 +6,8 @@ class Solution {
             if (nums[j] != nums[i - 1]) {
                 nums[i] = nums[j];
                 i++;
+            } else{
+                  continue;
             }
         }
 
